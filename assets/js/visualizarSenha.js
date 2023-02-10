b52737061e83454bfbb2de46e134c30e23fd6fa0
@Gollpy <ilanmg.14@gmail.com>
2023-02-10 18:41:38 -0300

    teste

--- assets/js/visualizarSenha.js
+++ assets/js/visualizarSenha.js
@@ -18,11 +18,5 @@ document.querySelector('.visualizarSenha').onclick = function () {
     mostraOuEsconderSenha();
 }
 
-    const body = document.querySelector('body');
-    const footer = document.querySelector('footer');
 
-    if(body.clientHeight < screen.height){
-        footer.style.cssText = "position:fixed;bottom: 0;left: 0;";
-        console.log('cheque');
-    }
 
