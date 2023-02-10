d8bf7b20333137189bb5da3ffd0afdd5b37807d6
@Gollpy <ilanmg.14@gmail.com>
2023-02-10 18:37:43 -0300

    teste

--- assets/js/visualizarSenha.js
+++ assets/js/visualizarSenha.js
@@ -1,8 +1,8 @@
-    const campoSenha = document.querySelector('#password');
-    const visualizarSenha = document.querySelector('.visualizarSenha');
 
     function mostraOuEsconderSenha () {
-        
+        const campoSenha = document.querySelector('#password');
+        const visualizarSenha = document.querySelector('.visualizarSenha');
+
         if (campoSenha.type === 'password'){
             visualizarSenha.innerHTML = 'visibility';
             campoSenha.type = 'text';
@@ -18,4 +18,11 @@ document.querySelector('.visualizarSenha').onclick = function () {
     mostraOuEsconderSenha();
 }
 
+    const body = document.querySelector('body');
+    const footer = document.querySelector('footer');
+
+    if(body.clientHeight < screen.height){
+        footer.style.cssText = "position:fixed;bottom: 0;left: 0;";
+        console.log('cheque');
+    }
 
