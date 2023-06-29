800354167d4278cace9041f036e5f5e5482d766f
@Gollpy <ilanmg.14@gmail.com>
2023-06-28 21:06:26 -0300

    melhorias

--- js/scriptForm.js
+++ js/scriptForm.js
@@ -13,18 +13,4 @@ function mostraEsconderSenha () {
         }
     });
 }
-
-// function janelaRecuperarSenha () {
-//     const botao = document.querySelectorAll('.abrir');
-//     const styleDisplay = document.querySelector('.section-recuperar-senha');
-//     botao.forEach(botao => {
-//         botao.addEventListener('click', () => {
-//             if (styleDisplay.style.display === 'none'){
-//                 styleDisplay.style.display = 'flex';
-//             } else {
-//                 styleDisplay.style.display = 'none';
-//             }
-//         })
-//     })
-// }
 mostraEsconderSenha();
