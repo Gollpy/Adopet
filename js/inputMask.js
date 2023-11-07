30c06da5a97423c1c6235ab39cb96fd7b1c1ad93
@Gollpy <ilanmg.14@gmail.com>
2023-11-06 22:50:54 -0300

    plicando inputMask para telefone

--- js/inputMask.js
+++ js/inputMask.js
@@ -1,5 +1,23 @@
-input.addAdventListen('click', Event =>{})
 
-function capture (){
-    const input = document.querySelector
+function mascaraTelefone (event) {
+    const input = event.target
+    const value = input.value.replace(/\D/g,'')
+    const formattedValue = formPhoneNumber(value)
+    
+    input.value = formattedValue
 }
+
+function formPhoneNumber (value) {
+    if(value.length === 2) {
+        return value.replace(/(\d{2})(\d{1,4})?(\d{4})?/,'$1')
+    } else if(value.length <= 6) {
+        return value.replace(/(\d{2})(\d{4})?(\d{4})?/,'($1) $2')
+    } else if(value.length <= 10) {
+        return value.replace(/(\d{2})(\d{4})?(\d{4})?/,'($1) $2-$3')
+    } else {
+        return value.replace(/(\d{2})(\d{5})?(\d{4})?/,'($1) $2-$3')
+    }
+}
+
+const telefoneInput = document.getElementById('tel')
+telefoneInput.addEventListener('input', mascaraTelefone)
